8de6f2a02d902eb98782acb2e38bc0c44f2e162f
Fabio Lo Presti <f.lopresti5@campus.unimib.it>
2026-01-26 08:43:05 +0100

    refactor(documents): ristruttura la cartella dei documenti per evitare duplicazioni di codice latex e dividi il documento "specifications" in "analysis" e "design"

--- documents/build.bat
+++ documents/build.bat
@@ -1,16 +1,24 @@
 @ECHO off
 
+
+REM validate argument
+IF "%~1"=="" (
+    ECHO Usage: build.bat ^<tex file name^>.tex
+	PAUSE
+    EXIT /b 1
+)
+
 REM variables
-SET MAINFILE=main.tex
+SET "MAINFILE=%~1"
 SET PASSES=2
+SET BUILD_DIR="build"
 
 REM Compile multiple times
 FOR /L %%i IN (1,1,%PASSES%) DO (
     ECHO "Running pdflatex (pass %%i)..."
-    PDFLATEX -interaction=nonstopmode "%MAINFILE%"
+    PDFLATEX -interaction=nonstopmode -output-directory="%BUILD_DIR%" "%MAINFILE%"
     IF ERRORLEVEL 1 (
         ECHO Error: pdflatex failed on pass %%i with code %ERRORLEVEL%.
-        PAUSE
         EXIT /b %ERRORLEVEL%
     )
 )
@@ -19,9 +27,8 @@ ECHO Compilation succeeded.
 
 REM clean up
 ECHO Cleaning up auxiliary files...
+CD "%BUILD_DIR%"
 DEL *.aux *.log *.toc
 
 
 ECHO Build completed successfully!
-
-PAUSE
