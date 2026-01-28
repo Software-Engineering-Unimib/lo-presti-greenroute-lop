00b4022618a38999d62d329550df5139cba7a699
Fabio Lo Presti <f.lopresti5@campus.unimib.it>
2026-01-28 16:14:46 +0100

    feat(server): crea il microservizio gateway e modifica il client in modo che richieda le tile da esso. Il microservizio riindirizzerà la richiesta a operstreetmap.org

--- client/react_native/App.js
+++ client/react_native/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { MapView, Camera } from "@maplibre/maplibre-react-native";
+import { MapView, Camera, RasterSource, RasterLayer} from "@maplibre/maplibre-react-native";
+
+serverURL = "10.0.2.2";
+serverPort = 80;
 
 export default function App() {
   return (
     <MapView
       style={StyleSheet.absoluteFill}
-      styleURL="https://demotiles.maplibre.org/style.json"
     >
       <Camera
         zoomLevel={10}
-        centerCoordinate={[9.185,  	45.465]}
+        centerCoordinate={[9.185, 45.465]}
       />
+      
+      <RasterSource
+        id="osm-proxy"
+        tileUrlTemplates={[`http://${serverURL}:${serverPort}/tiles/{z}/{x}/{y}.png`]}
+        tileSize={256}
+      > 
+        <RasterLayer
+          id="osm-layer"
+          sourceID="osm-proxy"
+        />
+      </RasterSource>
     </MapView>
   );
 }
