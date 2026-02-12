d4070767c16c54bd005570576e9c27b3ba5c61a7
Fabio Lo Presti <f.lopresti5@campus.unimib.it>
2026-02-12 19:14:37 +0100

    refactor(server): rimuovi un wrapper async non necessario

--- server/microservices/typescript_services/route_service/src/start.ts
+++ server/microservices/typescript_services/route_service/src/start.ts
@@ -3,8 +3,6 @@ import { RouteServer } from './RouteServer';
 
 // vero solo se questo file è stato eseguito direttamente
 if(import.meta.url === `file://${process.argv[1]}`){
-  (async () => {
     const server = new RouteServer();
     await server.start();
-  })();
 }
